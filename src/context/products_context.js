c273675bbdccff2fa19fd33f4443314978325dcf
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2023-12-10 09:43:30 +0100

    Closesidebar functionality implemented

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -23,14 +23,16 @@ export const ProductsProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   const openSidebar = () => {
-    dispatch({type: 'SIDEBAR_OPEN'})
+    dispatch({type: SIDEBAR_OPEN})
   }
-  useEffect(() => {
-    openSidebar()
-  }, [])
+
+   const closeSidebar = () => {
+    dispatch({type: SIDEBAR_CLOSE})
+  }
+ 
 
   return (
-    <ProductsContext.Provider value='products context'>
+    <ProductsContext.Provider value={{...state, openSidebar, closeSidebar}}>
       {children}
     </ProductsContext.Provider>
   )
