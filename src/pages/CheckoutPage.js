40dd665f604e700906356c59cbf32c35a3199baa
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2023-12-10 10:40:23 +0100

    About page, pageHero component, and checkout page added

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
-import { PageHero, StripeCheckout } from '../components'
+import { PageHero} from '../components'
+
 // extra imports
-import { useCartContext } from '../context/cart_context'
 import { Link } from 'react-router-dom'
 
 const CheckoutPage = () => {
-  return <h4>checkout page</h4>
+  
+
+  return (
+    <main>
+      <PageHero title='checkout' />
+      <Wrapper className='page'>        
+          <div className='empty'>
+            <h2>Your cart is empty</h2>
+            <Link to='/products' className='btn'>
+              fill it
+            </Link>
+          </div>
+        
+      </Wrapper>
+    </main>
+  )
 }
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .empty {
+    text-align: center;
+  }
+`
 export default CheckoutPage
