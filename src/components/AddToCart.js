d5e43f9f2d7f797b564a4fb571d74cf0174b710c
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2023-12-11 10:59:27 +0100

    Colors added to AddToCart button component

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -3,12 +3,38 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { FaCheck } from 'react-icons/fa'
 import { useCartContext } from '../context/cart_context'
-import AmountButtons from './AmountButtons'
 
-const AddToCart = () => {
-  return <h4>addToCart </h4>
+const AddToCart = ({ product }) => {
+  // add to cart
+
+  const { id, colors } = product
+  const [mainColor, setMainColor] = useState(colors[0])
+  
+ 
+  return (
+    <Wrapper>
+      <div className='colors'>
+        <span>colors :</span>
+        <div>
+          {colors.map((color, index) => {
+            return (
+              <button
+                key={index}
+                style={{ background: color }}
+                className={`${
+                  mainColor === color ? 'color-btn active' : 'color-btn'
+                }`}
+                onClick={() => setMainColor(color)}
+              >
+                {mainColor === color ? <FaCheck /> : null}
+              </button>
+            )
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
-
 const Wrapper = styled.section`
   margin-top: 2rem;
   .colors {
