2bea169af69f23355ed5a687336dedc7659f95ad
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2023-12-11 11:01:15 +0100

    Update AmountButons component

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -3,14 +3,32 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { FaCheck } from 'react-icons/fa'
 import { useCartContext } from '../context/cart_context'
-
+import AmountButtons from './AmountButtons'
 const AddToCart = ({ product }) => {
   // add to cart
-
-  const { id, colors } = product
+  const { addToCart } = useCartContext()
+  const { id, stock, colors } = product
   const [mainColor, setMainColor] = useState(colors[0])
-  
- 
+  const [amount, setAmount] = useState(1)
+
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1
+      if (tempAmount > stock) {
+        tempAmount = stock
+      }
+      return tempAmount
+    })
+  }
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1
+      if (tempAmount < 1) {
+        tempAmount = 1
+      }
+      return tempAmount
+    })
+  }
   return (
     <Wrapper>
       <div className='colors'>
@@ -31,6 +49,21 @@ const AddToCart = ({ product }) => {
             )
           })}
         </div>
+      </div>
+      <div className='btn-container'>
+        <AmountButtons
+          increase={increase}
+          decrease={decrease}
+          amount={amount}
+        />
+
+        <Link
+          to='/cart'
+          className='btn'
+          onClick={() => addToCart(id, mainColor, amount, product)}
+        >
+          add to cart
+        </Link>
       </div>
     </Wrapper>
   )
