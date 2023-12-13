d316e8482d435e745c684e5ac5a4a8cd64911134
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2023-12-13 19:56:02 +0100

    Auth0 functionality added to cartButton component

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -8,7 +8,7 @@ import { useUserContext } from '../context/user_context'
 const CartButton = () => {
   const { closeSidebar } = useProductsContext()
   const { total_items, clearCart } = useCartContext()
-  
+  const { loginWithRedirect, myUser, logout } = useUserContext()
   return (
     <Wrapper className='cart-btn-wrapper'>
       <Link to='/cart' className='cart-btn' onClick={closeSidebar}>
@@ -18,22 +18,23 @@ const CartButton = () => {
           <span className='cart-value'>{total_items}</span>
         </span>
       </Link>
-      
+      {myUser ? (
         <button
           type='button'
           className='auth-btn'
           onClick={() => {
             clearCart()
             localStorage.removeItem('user')
-         }}
+            logout({ returnTo: window.location.origin })
+          }}
         >
           Logout <FaUserMinus />
         </button>
-      
-        <button type='button' className='auth-btn'>
+      ) : (
+        <button type='button' className='auth-btn' onClick={loginWithRedirect}>
           Login <FaUserPlus />
         </button>
-      
+      )}
     </Wrapper>
   )
 }
