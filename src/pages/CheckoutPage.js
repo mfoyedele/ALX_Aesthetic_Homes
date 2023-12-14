9f66efe0635b355010f57187647a59c98e51b2ab
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2023-12-14 07:42:51 +0100

    Checkout page functionality added

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
-import { PageHero} from '../components'
+import { PageHero, StripeCheckout } from '../components'
 
 // extra imports
+
+import { useCartContext } from '../context/cart_context'
 import { Link } from 'react-router-dom'
 
 const CheckoutPage = () => {
-  
+  const { cart } = useCartContext()
 
   return (
     <main>
       <PageHero title='checkout' />
-      <Wrapper className='page'>        
+      <Wrapper className='page'>
+        {cart.length < 1 ? (
           <div className='empty'>
             <h2>Your cart is empty</h2>
             <Link to='/products' className='btn'>
               fill it
             </Link>
           </div>
-        
+        ) : (
+          <StripeCheckout />
+        )}
       </Wrapper>
     </main>
   )
