5dff56dcc774f82717f5fff63170f62935c6543e
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2023-12-21 07:36:13 +0100

    Image removed from homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,12 +3,7 @@ import { FeaturedProducts, Hero, Services, Contact } from '../components'
 const HomePage = () => {
   return (
     <main>
-      <Hero />
-      <div>
-        <p>lorem5lorememem</p>
-        <img src="https://www.course-api.com/images/store/product-2.jpeg" alt="Girl in a jacket" width="500" height="600"/>
-
-      </div>
+      <Hero />     
       <FeaturedProducts />
       <Services />
       <Contact />
