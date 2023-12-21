a0a64fcfcb94f819e19ff186b584544d82b5836e
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2023-12-21 07:35:11 +0100

    Update homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,6 +4,11 @@ const HomePage = () => {
   return (
     <main>
       <Hero />
+      <div>
+        <p>lorem5lorememem</p>
+        <img src="https://www.course-api.com/images/store/product-2.jpeg" alt="Girl in a jacket" width="500" height="600"/>
+
+      </div>
       <FeaturedProducts />
       <Services />
       <Contact />
